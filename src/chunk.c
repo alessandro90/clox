dd3a5ec017dc4fd7ebd60c546f1c1ee8bb721304
alessandro90 <alessandro.ceccato.3@gmail.com>
2023-07-19 19:42:55 +0200

    Adding to the constant table Ch 26.7.1

--- src/chunk.c
+++ src/chunk.c
@@ -1,5 +1,7 @@
 #include "chunk.h"
+
 #include "memory.h"
+#include "vm.h"
 #include <stdlib.h>
 
 void initChunk(Chunk *chunk) {
@@ -30,6 +32,8 @@ void writeChunk(Chunk *chunk, u8 byte, usize line) {
 }
 
 usize addConstant(Chunk *chunk, Value value) {
+    push(value);
     writeValueArray(&chunk->constants, value);
+    pop();
     return chunk->constants.count - 1U;
 }
